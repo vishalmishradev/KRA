1bc371babf2c534d4e58470f3ea0237e85373544
vishalmishradev <vishalmishra@medianv.com>
2026-02-19 14:25:16 +0530

    edited the server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res)=>{
+    res.send("Welcome to my page")
+})
 
 app.listen(PORT, ()=>{
     console.log("Server is up and running on port: ", PORT);
