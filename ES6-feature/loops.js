a53cf70a29922eb1e79b14346fbfc485d76c89e4
vishalmishradev <vishalmishra@medianv.com>
2026-02-19 15:54:02 +0530

    practiced looping

---
+++ ES6-feature/loops.js
@@ -0,0 +1,26 @@
+// there are generally three types of loops that are used and they are for loop, while loop and do-whileloop;
+
+//for loop 
+
+console.log("Example of for loop");
+for (let i = 1; i < 5; i++) {
+  console.log("Number:", i);
+}
+
+
+//while loop(entry control loop)
+
+console.log("Example of while loop");
+let j = 1;
+while (j < 5) {
+  console.log("Number:", j);
+  j++;
+}
+
+// do-while loop(exit control loop)
+console.log("Example of do-while loop");
+let k = 0;
+do {
+  console.log("Number:", k);
+  k++;
+} while (k < 5);
