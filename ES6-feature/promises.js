2d6999d1bab051a929326a3b047745f1cade605c
vishalmishradev <vishalmishra@medianv.com>
2026-02-19 16:11:30 +0530

    practiced promise

---
+++ ES6-feature/promises.js
@@ -0,0 +1,24 @@
+// A Promise in JavaScript is used to handle async operations like API calls, timers, file loading, etc.
+// It has 3 states:
+
+// Pending, fulfilled, rejected
+
+let promise = new Promise(function(resolve, reject) {
+
+  let success = false;
+
+  if (success) {
+    resolve("Task completed");
+  } else {
+    reject("Task failed");
+  }
+
+});
+
+promise
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
