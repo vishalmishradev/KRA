dd7a33545cf4f3fb8c8eb62de2bfb94107c7433d
vishalmishradev <vishalmishra@medianv.com>
2026-02-19 14:35:45 +0530

    Js object practiced

---
+++ js-methods/object.js
@@ -0,0 +1,13 @@
+const obj ={
+    name:"Vishal Mishra",
+    age:"27",
+    adress : {
+        city: "Jamshedpur",
+        state: "Jharkhand"
+    }
+}
+
+
+console.log(Object.keys(obj)); // return array of keys
+console.log(Object.values(obj)); // return array of values
+console.log(Object.entries(obj));  // return array key:value pair
